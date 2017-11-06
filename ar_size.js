0493b514124ad1aa7449c8261829d1b2f1674a43
pollenjp <polleninjp@gmail.com>
2017-11-06 22:05:51 +0900

    Complete the dynamic change of the image size.
    In this commit, when you reload the page, you can see the appropriate
    size.
    If you want the really dynamic change(when you change the page size by
    dragging the brower's corner, the image trace the change), you can do
    it.

--- ar_size.js
+++ ar_size.js
@@ -62,7 +62,9 @@ function styleMargin(elem, x1, x2, x3, x4){
     elem.style.marginBottom=x3;
     elem.style.marginLeft  =x4;
 }
-
+/********************************************************
+  *******************************************************
+  *******************************************************/
 // css
 var arId = document.getElementById("ar");
 arId.style.boxSizing = "border-box";
@@ -82,51 +84,172 @@ containerClass[0].style.backgroundColor="rgba(95, 168, 160, 0.5)";
 //    padding: 20px;
 //    background-color: rgba(95, 168, 160, 0.5);
 //}
-/* #ar .container .block0 */
-var block0Class = arId.getElementsByClassName("block0");       //block: length 2
-pw=getComputedStyle( containerClass[0] ).width;               //parent width
-ppr=getComputedStyle( containerClass[0] ).paddingTop;         // parent padding right ( = left )
-w=(pw-ppr*2)/2;
-for(var i=0; i<block0Class.length; i++){
-    styleWidth(block0Class[i], w);
+/******************************************************************
+ ****************** #ar .container .block0 ************************
+ ******************************************************************/
+if( window.matchMedia('(max-width:670px)').matches ){
+    // smart phone
+    //@media only screen and (max-width: 670px) {
+    //    /* #ar .container .block0 */
+    //    #ar .block0
+    //    {
+    //        width: calc( 100% - 20px - 20px );
+    //    }
+    //}
+    var block0Class = arId.getElementsByClassName("block0");        //block: length 2
+    ps = getComputedStyle( containerClass[0] );                     // Parent Style // parent="#ar .container"
+    pw = Number(ps.width.split("p")[0]);                            // Parent Width
+    ppr = Number(ps.paddingRight.split("p")[0]);                    // Parent PaddingRight
+    cw = (pw - 2*ppr).toString() + "px";                                   // Current Width
+    for(var i=0; i<block0Class.length; i++){
+        styleWidth(block0Class[i], cw);
+    }
+} else if(window.matchMedia('(max-width:1000px)').matches ){
+    //@media only screen and (max-width: 1000px) {
+    //    /* #ar .container .block0 */
+    //    #ar .block0
+    //    {
+    //        width: calc( 100% - 20px - 20px );
+    //    }
+    //}
+    var block0Class = arId.getElementsByClassName("block0");        //block: length 2
+    ps = getComputedStyle( containerClass[0] );                     // Parent Style // parent="#ar .container"
+    pw = Number(ps.width.split("p")[0]);                            // Parent Width
+    ppr = Number(ps.paddingRight.split("p")[0]);                    // Parent PaddingRight
+    cw = (pw - 2*ppr).toString() + "px";                                   // Current Width
+    for(var i=0; i<block0Class.length; i++){
+        styleWidth(block0Class[i], cw);
+    }
+} else {
+    //#ar .block0
+    //{
+    //    width: calc( (100% - 20px - 20px)/2);
+    //    float: left;
+    //    margin: 10px;
+    //    padding: 10px;
+    //}
+    var block0Class = arId.getElementsByClassName("block0");        //block: length 2
+    ps = getComputedStyle( containerClass[0] );                     // Parent Style // parent="#ar .container"
+    pw = Number(ps.width.split("p")[0]);                            // Parent Width
+    ppr = Number(ps.paddingRight.split("p")[0]);                    // Parent PaddingRight
+    cw = ((pw-ppr*2)/2).toString() + "px";                          // Current Width
+    for(var i=0; i<block0Class.length; i++){
+        styleWidth(block0Class[i], cw);
+    }
 }
-//#ar .block0
-//{
-//    width: calc( (100% - 20px - 20px)/2);
-//    float: left;
-//    margin: 10px;
-//    padding: 10px;
-//}
-var block0leftClass = arId.getElemensByClassName("block0 left");
+
+var clear0leftClass = arId.getElementsByClassName("clear0 left");
+clear0leftClass[0].style.clear="left";
 ///* #ar .container .clear0.left */
 //#ar .clear0.left
 //{
 //    clear: left;
 //}
-//
+var block0BlueClass = arId.getElementsByClassName("block0 blue");
+block0BlueClass[0].style.backgroundColor = "rgba(174, 232, 246, 0.6)";
 ///* #ar .container .block0.blue */
 //#ar .block0.blue
 //{
 //    background-color: rgba(174, 232, 246, 0.6);
 //}
-//
+var block0GreenClass = arId.getElementsByClassName("block0 green");
+block0GreenClass[0].style.backgroundColor = "rgba(0, 255, 0, 0.6)";
 ///* #ar .container .block0.green */
 //#ar .block0.green
 //{
 //    background-color: rgba(0, 255, 0, 0.6);
 //}
-//
-///* #ar .container .block0 .block1 */
-//#ar .block1
-//{
-//    width: calc((100% - 10px - 10px)/2);
-//    float: left;
-//    margin: 5px;
-//    padding: 5px;
-//}
-//
+/***********************************************************
+ *************** #ar .container .block0 .block1 ***********
+ **********************************************************/
+if( window.matchMedia('(max-width:670px)').matches ){
+    // Smart Phone
+    //@media only screen and (max-width: 670px) {
+    //    /* #ar .container .block0 .block1 */
+    //    #ar .block1
+    //    {
+    //        width: calc( 100% - 20px - 20px );
+    //    }
+    //}
+    var block1Class = arId.getElementsByClassName("block1");
+    ps = getComputedStyle( block0Class[0] );                        // Parent Style // parent="#ar .block0"
+    pw = Number(ps.width.split("p")[0]);                            // Parent Width
+    ppr = Number(ps.paddingRight.split("p")[0]);                    // Parent PaddingRight
+    cw = (pw - 2*ppr).toString() + "px";                           // Current Width
+    for(var i=0; i<block1Class.length; i++){
+        styleWidth(block1Class[i], cw );
+        block1Class[i].style.float = "left";
+        styleMargin(block1Class[i], "5px", "5px", "5px", "5px");
+        stylePadding(block1Class[i], "5px", "5px", "5px", "5px");
+    }
+} else {
+    // Tablet, PC
+    //#ar .block1
+    //{
+    //    width: calc((100% - 10px - 10px)/2);
+    //    float: left;
+    //    margin: 5px;
+    //    padding: 5px;
+    //}
+    var block1Class = arId.getElementsByClassName("block1");
+    ps = getComputedStyle( block0Class[0] );                        // Parent Style // parent="#ar .block0"
+    pw = Number(ps.width.split("p")[0]);                            // Parent Width
+    ppr = Number(ps.paddingRight.split("p")[0]);                    // Parent PaddingRight
+    cw = ((pw-ppr*2)/2).toString() + "px";                          // Current Width
+    for(var i=0; i<block1Class.length; i++){
+        styleWidth(block1Class[i], cw );
+        block1Class[i].style.float = "left";
+        styleMargin(block1Class[i], "5px", "5px", "5px", "5px");
+        stylePadding(block1Class[i], "5px", "5px", "5px", "5px");
+    }
+}
+/*****************************************************************
+ *************** #ar .container .block0 .block1 .img *************
+ *****************************************************************/
+var markerImgClass = arId.getElementsByClassName("marker-img");
+ps = getComputedStyle( block1Class[0] );
+pw = Number(ps.width.split("p")[0]);                // String --> remove "px" --> Number
+ppr = Number(ps.paddingRight.split("p")[0]);
+//console.log(pw);
+//console.log(typeof pw);
+//console.log(pw.split("p")[0]);
+//console.log(Number(pw.split("p")[0]));
+//console.log(typeof pw);
+cw = (pw - 2*ppr).toString() + "px";
+console.log(cw);
+for(var i=0; i<markerImgClass.length; i++){
+    styleWidth( markerImgClass[i], cw);
+    markerImgClass[i].style.height=cw;
+}
+/****************************************************
+ ****************** #ar .clear1.left ***************
+ ***************************************************/
+if( window.matchMedia("(max-width:670px)").matches ){
+    //@media only screen and (max-width: 670px) {
+    //    #ar .clear1.left
+    //    {
+    //        clear: left;
+    //    }
+    //}
+    var clear1Class= arId.getElementsByClassName("clear1 left");
+    clear1Class[clear1Class.length-1].style.clear = "left";
+}
+var endClass = arId.getElementsByClassName("end");
+endClass[0].style.clear = "both";
 //#ar .end
 //{
 //    clear: both;
 //}
 //
+//@media only screen and (max-width: 670px) {
+//    /* #ar .container .block0 .block1 */
+//    #ar .block1
+//    {
+//        width: calc( 100% - 20px - 20px );
+//    }
+//    /* #ar .container .block0 .clear1.left */
+//    #ar .clear1.left
+//    {
+//        clear: left;
+//    }
+//}
