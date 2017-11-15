dea0d2041c207100b18809497dff980e30927f24
yosi <uehara@gatari.co.jp>
2017-11-15 16:40:01 +0900

    added a slide changed by time

--- js/top.js
+++ js/top.js
@@ -1,9 +1,25 @@
-// JavaScript Document
-$(function() {
-  $('.index-btn').click(function() {
-    $('.activeSlide').removeClass('activeSlide');
-    var clickedIndex = $('.index-btn').index($(this));
-    $('.slide').eq(clickedIndex).addClass('activeSlide');
-    
-  });
+$(function(){
+	var slide = "#slide-wrap";
+	var i = 0;
+	var pics = 2; //写真の枚数（0から数える）
+	$(slide).find("li").eq(0).show();
+	setInterval(function(){
+		if(i < pics){
+			setTimeout(function(){
+				$(slide).find("li").fadeOut(500);
+				i++;
+				setTimeout(function(){
+					$(slide).find("li").eq(i).fadeIn(500);
+				},500);
+			},1000);
+		}else if(i === pics){
+			setTimeout(function(){
+				$(slide).find("li").fadeOut(500);
+				i = 0;
+				setTimeout(function(){
+					$(slide).find("li").eq(i).fadeIn(500);
+				},500);
+			},1000);
+		}
+	},1500*(pics+1));
 });
